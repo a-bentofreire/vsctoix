948c96f6e0f0567470c17af85a7c4fd5af34e321
a-bentofreire <business@a-bentofreire.com>
2024-09-06 22:30:51 -0300

    Add translations support

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@
 
 import * as vscode from 'vscode';
 import * as utilityList from './common/utility-list';
+import * as l10n from '@vscode/l10n';
 
 // ------------------------------------------------------------------------
 //                               Startup Code
@@ -15,10 +16,10 @@ import * as utilityList from './common/utility-list';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-  for (const action of utilityList.utilityList) {
-    const disposable = vscode.commands.registerCommand(action.id, action.f);
-    context.subscriptions.push(disposable);
-  }
+    for (const action of utilityList.utilityList) {
+        const disposable = vscode.commands.registerCommand(action.id, action.f);
+        context.subscriptions.push(disposable);
+    }
 }
 
 // ------------------------------------------------------------------------
